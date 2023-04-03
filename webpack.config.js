eac4502802f2d39a1ec10f27603de91b8d8c92a8
Pavel <pavel8897@bk.ru>
2023-04-03 23:00:33 +0300

    first

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: ['@babel/polyfill', './src/index.jsx'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name].[hash].js"
+    },
+    devServer: {
+        port: 3000
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: "./src/index.html"}),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.(less|css)$/i,
+            use: [
+              // compiles Less to CSS
+              "style-loader",
+              "css-loader",
+              "less-loader",
+            ],
+          },
+          {
+            test: /\.(jpg|jpeg|png|svg)$/i,
+            use: [           
+              "file-loader"
+            ],
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+          {
+            test: /\.m?jsx$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ['@babel/preset-env', "@babel/preset-react"]
+              }
+            }
+          }
+        ],
+      },
+}
